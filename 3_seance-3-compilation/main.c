2f31dfa90c6a53c5225b1bfa06bc95f1fc7dafc0
SEHEUX Jean-Francois <jean.francois.seheux@gmail.com>
2025-11-07 00:57:14 +0100

    seance-3

---
+++ 3_seance-3-compilation/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define DIX 10
+
+int main(void)
+{
+    // commentaire de test
+    int i = 0;
+    if (i < DIX)
+    {
+    printf("Hello, World!\n");
+
+    }
+    return 0;
+}
