af0bc1be1fe83d25ed81b3e1e6ad2443e8b20c75
SEHEUX Jean-Francois <jean.francois.seheux@gmail.com>
2025-11-21 01:58:36 +0100

    correction seance 4 part 1
    (cherry picked from commit 5536a2879908ee5ed54e1000dd301e3e40524d1d)

--- 4_seance-4-pointeurs/1_pointeurs/main.c
+++ 4_seance-4-pointeurs/1_pointeurs/main.c
@@ -1,35 +1,97 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "main.h"
 
-int main()
+void addOne(int a)
 {
-    // La portee de a est cette fonction
-    int a = 10;
-    scopeExample();
-    return 0;
-}
-
-void multiplyByTwo(int a)
-{
-    a *= 2;
+    a += 1;
 }
 
 void scopeExample()
 {
-    // La portee de a est cette fonction
+    // la portee de a est cette fonction
     int a = 4;
-    multiplyByTwo(a);
+    addOne(a);
     printf("a = %d\n", a);
 
-    if (a <= 4)
+    if (a > 4)
     {
-        // La portee de b est ce block conditionnel
+        // la portee de b est ce block conditionnel
         int b = 10;
     }
 
     // printf("b = %d\n", b);
 
+}
+
+int main()
+{
+    // la portee de a est cette fonction
+    int a = 10;
+    int b = 2;
+    scopeExample();
+
+    showAddress(a);
+
+    int *pt = &a;
+    showAddress2(pt);
+
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+
+    swap(&a, &b);
+    printf("\nswap\n\n");
+
+    printf("a = %d\n", a);
+    printf("b = %d\n", b);
+
+    doubleint(&a);
+    printf("\nx2\n");
+    printf("a = %d\n", a);
+
+    return 0;
+}
+
+void pointers()
+{
+    int a = 3; // Une variable
+    int *pt = &a; // Un pointeur (initialisé à 0)
+    // Le pointeur a le type de la variable qu'il va pointer
+
+    pt = &a; // pt pointe vers a
+    // & donne l'addresse d'une variable
+
+    printf("La valeur de a est %d et son adresse est %p\n", *pt, pt);
+    // * dereference, elle donne la valeur pointee
+
+}
+
+
+// Affichez le contenue d'une variable et son addresse
+void showAddress(int a)
+{
+    printf("La variable a %d a pour addresse %p\n", a, &a);
+}
+
+// Affichez le contenue d'une variable et son addresse
+void showAddress2(int *pta)
+{
+    printf("La variable a %d a pour addresse %p\n", *pta, pta);
+}
+
+// Echanger le contenue de variables a et b
+void swap(int *pta, int *ptb)
+{
+    int tmp = *pta;
+    *pta = *ptb;
+    *ptb = tmp;
+}
+
+// Ecrire une fonction de type void qui double la valeur d'un int
+void doubleint(int *a)
+{
+    *a +=*a;
 }
 
 
--- 4_seance-4-pointeurs/2_tableaux/main.c
+++ 4_seance-4-pointeurs/2_tableaux/main.c
@@ -56,6 +56,86 @@ int main(void)
     // passer un tableau en parametre d'une fonction
     // function(int tab[])
     // function(int * tab)
+}
+
+
+int main(void)
+{
+    createAndPrint(6);
+
+    int tab[] = {1, 22, -3, 34, 5};
+    if (exitInTab(tab, 5, 34))
+    {
+        printf("34 est present\n");
+    }
+    if (!exitInTab(tab, 5, 112))
+    {
+        printf("112 n'est pas present\n");
+    }
+
+    invertTabElements(tab, 5);
+
+
+
 
     return 0;
+}
+
+// Ecrire une fonction qui initialise un tableau de 10 éléments.
+// Le premier élément prend la valeur `start`, chaque élément est égale au précédent +1.
+// Affichez le tableau puis la somme des éléments du tableau.
+void createAndPrint(int start)
+{
+    int tableau[10];
+    int somme = 0;
+
+    // Initialisation et calcul de la somme
+    for (int i = 0; i < 10; i++) {
+        tableau[i] = i + start;
+        somme += tableau[i];
+    }
+
+    // Affichage des elements
+    for (int i = 0; i < 10; i++) {
+        printf("Element %d : %d\n", i, tableau[i]);
+    }
+
+    // Affichage de la somme
+    printf("Somme des elements : %d\n", somme);
+}
+
+// Ecrire une fonction qui recherche dans un tableau passé en paramètre si une valeur existe.
+bool exitInTab(int *tab, int size, int value)
+{
+    for (int i = 0; i < size; i++) {
+        if (tab[i] == value) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool exitInTabRecurse(int *tab, int size, int value)
+{
+    if (size <= 0) return false;
+    if (value == *tab) return true;
+    return exitInTabRecurse(tab+1, size-1, value);
+}
+
+// Sans creer de tableau suplementaire, inversez les éléments d'un tableau puis affichez les.
+void invertTabElements(int *tab, int size)
+{
+    // Inversion du tableau
+    for (int i = 0; i < size / 2; i++) {
+        int tmp = tab[i];
+        tab[i] = tab[size - i - 1];
+        tab[size - i - 1] = tmp;
+    }
+
+    // Affichage du tableau inverse
+    printf("Tableau inverse : ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", tab[i]);
+    }
+    printf("\n");
 }
